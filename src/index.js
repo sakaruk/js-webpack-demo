85f7e549ee5a512597d147a2117e5919580b50a8
Sakar Upadhyaya Khatiwada <urssakar@gmail.coM>
2024-12-20 13:23:13 +0545

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { greet } from './module1';
+
+const message = greet('Students');
+console.log(message);
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true, // Cleans the output directory before building
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    open: true,
+    port: 8080,
+  },
+};
